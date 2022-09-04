b46d6feee0243696141429788d1cd1ebb44dae69
eomth0212 <eomth0212@naver.com>
2022-09-04 17:09:09 +0900

    CSS,JS. navbar fixed

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ document.addEventListener('scroll', () => {
     } 
 });
 
+
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
@@ -19,8 +20,17 @@ navbarMenu.addEventListener('click', (event) => {
     if (link == null) {
         return;
     }
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
 }); 
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+navbarMenu.classList.toggle('open');
+});
+
+
 
 // Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector('.home__contact')
@@ -64,11 +74,12 @@ workBtnContainer.addEventListener('click', (e) => {
         return;
     }
 
-    // Remone selected from the previous item and select the new one
+    // Remove selected from the previous item and select the new one
     const active = document.querySelector('.category__btn.selected');
+    if (active != null) {
     active.classList.remove('selected');
-    const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
-    target.classList.add('selected');
+    }
+    e.target.classList.add('selected');
 
     projectContainer.classList.add('anim-out');
     setTimeout(() => {
